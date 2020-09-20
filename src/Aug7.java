2818007bb47fcbba7684ed96da484843fd105368
charming-c <822170415@qq.com>
2020-09-20 21:52:35 +0800

    9.20

--- src/Aug7.java
+++ src/Aug7.java
@@ -12,27 +12,27 @@ public class Aug7 {
       }
     }
     public ListNode detectCycle(ListNode head) {
-        ListNode fast=head;
-        ListNode slow=head;
+        ListNode fast = head;
+        ListNode slow = head;
         if(head==null){
             return null;
         }else if(head.next==null){
             return null;
         }
-        while(fast!=null&&fast.next!=null){
+        while(fast != null && fast.next!=null){
             fast=fast.next.next;
             slow=slow.next;
             if(fast==slow)
                 break;
         }
-        if(fast!=slow)
+        if(fast != slow)
             return null;
         fast=head;
         /*
         * 这里为什么要让快指针回到开头并且速度变为和慢指针一样呢？
         *
         * 由前面可知，快指针的速度是蛮指针的两倍，------（1）
-        * 而两者能相遇，快指针一定比慢指针多在环里跑了一圈（这里针对慢指针每次只走一步，快指针每次多走一步），
+        * 而两者能相遇，快指针一定比慢指针多在环里跑了n圈（这里针对慢指针每次只走一步，快指针每次多走一步），
         * 又由于（1），所以慢指针走的路程也是一个环的路程
         * 让快慢指针一步一步走，快指针从头走，慢指针接着走
         * 走过链表头到环头的距离
---
+++ src/两两交换链表中的节点.java
@@ -0,0 +1,31 @@
+/*
+* 给定一个链表，两两交换其中相邻的节点，并返回交换后的链表。
+
+你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+
+ 
+
+示例:
+
+给定 1->2->3->4, 你应该返回 2->1->4->3.
+
+* */
+
+public class 两两交换链表中的节点 {
+    public Aug3.ListNode swapPairs(Aug3.ListNode head) {
+        if(head == null || head.next == null)
+            return head;
+
+        Aug3.ListNode firstNode = head;
+        Aug3.ListNode secondNode = head.next;
+
+        firstNode = swapPairs(secondNode);
+        firstNode.next  = swapPairs(secondNode.next);
+        secondNode.next = firstNode;
+
+        return secondNode;
+
+    }
+
+}
+
