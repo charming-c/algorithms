1a2880c471efc6d741a48d05f1af342cff158af2
charming-c <822170415@qq.com>
2020-11-13 00:46:16 +0800

    11.12

---
+++ src/extend/Employee.java
@@ -0,0 +1,48 @@
+package extend;
+
+import java.time.LocalDate;
+
+public class Employee {
+
+    private String name;
+    private double salary;
+    private LocalDate hireDay;
+
+
+    //构造方法，用于初始化类的对象
+    public Employee(String name, double salary, int year, int month, int day) {
+        this.name = name;
+        this.salary = salary;
+        this.hireDay = LocalDate.of(year, month, day);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public LocalDate getHireDay() {
+        return hireDay;
+    }
+
+    public void setHireDay(LocalDate hireDay) {
+        this.hireDay = hireDay;
+    }
+
+    public void raiseSalary(double byPercent) {
+        double raise = salary * byPercent / 100;
+        salary += raise;
+    }
+
+}
---
+++ src/extend/Manager.java
@@ -0,0 +1,9 @@
+package extend;
+
+public class Manager extends Employee {
+
+    public Manager(String name, double salary, int year, int month, int day) {
+        super(name, salary, year, month, day);
+    }
+
+}
