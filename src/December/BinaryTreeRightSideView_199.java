1a1a5bd9ed620f7ab9d18da356ba8bc74d577f7e
charming-c <822170415@qq.com>
2020-12-07 23:46:50 +0800

    12.07

---
+++ src/December/BinaryTreeRightSideView_199.java
@@ -0,0 +1,54 @@
+package December;
+
+//给定一棵二叉树，想象自己站在它的右侧，按照从顶部到底部的顺序，返回从右侧所能看到的节点值。
+//
+//        示例:
+//
+//        输入: [1,2,3,null,5,null,4]
+//        输出: [1, 3, 4]
+//        解释:
+//
+//        1            <---
+//        /   \
+//        2     3         <---
+//        \     \
+//        5     4       <---
+
+import November.对称二叉树;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class BinaryTreeRightSideView_199 {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    // 广度优先算法
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        if (root == null) return list;
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if (node.left != null) queue.offer(node.left);
+                if (node.right != null) queue.offer(node.right);
+                if (i == size - 1)
+                    list.add(node.val);
+            }
+        }
+        return list;
+    }
+}
