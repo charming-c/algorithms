6d34f97c9738523715034239b8d87b201cdce506
charming-c <822170415@qq.com>
2021-01-10 20:46:14 +0800

    1.10

--- src/extend/Test.java
+++ src/extend/Test.java
@@ -1,19 +1,20 @@
 package extend;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Test {
 
     public static void main(String []args) throws ClassNotFoundException, NoSuchMethodException {
 
-        Employee e;
-        Apple apple = new Apple();
-        Manager staff2 = new Manager("b",5000,2020,11,13);
-
-        e = staff2;
-        ArrayList<Employee> list = new ArrayList<>();
-        list.add(e);
-        list.add(staff2);
+//        Employee e;
+//        Apple apple = new Apple();
+//        Manager staff2 = new Manager("b",5000,2020,11,13);
+//
+//        e = staff2;
+//        ArrayList<Employee> list = new ArrayList<>();
+//        list.add(e);
+//        list.add(staff2);
 //        for(Employee i:list){
 //            System.out.println(i.getName());
 //        }
@@ -22,13 +23,13 @@ public class Test {
 //        integer.intValue();
 //        System.out.println(integer.toString());
 
-        System.out.println(e.getClass());
-        System.out.println(e instanceof Employee);
-        System.out.println(e instanceof Manager);
-        System.out.println(staff2 instanceof Employee);
-
-
-        Person p = new Person();
+//        System.out.println(e.getClass());
+//        System.out.println(e instanceof Employee);
+//        System.out.println(e instanceof Manager);
+//        System.out.println(staff2 instanceof Employee);
+//
+//
+//        Person p = new Person();
 //        p.eat(new Apple());
 //        p.eat(new Banana());
 //        p.eat(new Peach());
@@ -38,7 +39,7 @@ public class Test {
 //                return "unknown";
 //            }
 //        });
-        p.eat(()->{return "00";});
+//        p.eat(()->{return "00";});
 //        Class class1 = p.getClass();
 //        Class class2 = Person.class;
 //        Class class3 = Class.forName("extend.Person");
@@ -69,6 +70,9 @@ public class Test {
 //        System.out.println(example.getClass());
 //        System.out.println(example.hashCode());
 //        System.out.println(example.toString());
+        Random a = new Random();
+        int b = a.nextInt(250);
+        System.out.println(b);
     }
 
 }
