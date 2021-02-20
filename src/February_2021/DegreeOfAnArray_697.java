81239670234c18571515ac1f2ed6b0ac67711ee5
charming-c <822170415@qq.com>
2021-02-20 15:02:21 +0800

    2.20

--- src/February_2021/DegreeOfAnArray_697.java
+++ src/February_2021/DegreeOfAnArray_697.java
@@ -17,7 +17,9 @@ package February_2021;
 //        最短连续子数组[2, 2]的长度为2，所以返回2.
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class DegreeOfAnArray_697 {
 
@@ -69,5 +71,43 @@ public class DegreeOfAnArray_697 {
             right++;
         }
         return right - left + 1;
+    }
+
+//    官方题解：（还是人家写得妙）
+//    记原数组中出现次数最多的数为 xx，那么和原数组的度相同的最短连续子数组，
+//    必然包含了原数组中的全部 xx，且两端恰为 xx 第一次出现和最后一次出现的位置。
+//
+//    因为符合条件的 xx 可能有多个，即多个不同的数在原数组中出现次数相同。
+//    所以为了找到这个子数组，我们需要统计每一个数出现的次数，同时还需要统计每一个数第一次出现和最后一次出现的位置。
+//
+//    在实际代码中，我们使用哈希表实现该功能，每一个数映射到一个长度为 33 的数组，
+//    数组中的三个元素分别代表这个数出现的次数、这个数在原数组中第一次出现的位置和这个数在原数组中最后一次出现的位置。
+//    当我们记录完所有信息后，我们需要遍历该哈希表，找到元素出现次数最多，且前后位置差最小的数。
+
+
+    public int findShortestSubArray2(int[] nums) {
+        Map<Integer, int[]> map = new HashMap<Integer, int[]>();
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            if (map.containsKey(nums[i])) {
+                map.get(nums[i])[0]++;
+                map.get(nums[i])[2] = i;
+            } else {
+                map.put(nums[i], new int[]{1, i, i});
+            }
+        }
+        int maxNum = 0, minLen = 0;
+        for (Map.Entry<Integer, int[]> entry : map.entrySet()) {
+            int[] arr = entry.getValue();
+            if (maxNum < arr[0]) {
+                maxNum = arr[0];
+                minLen = arr[2] - arr[1] + 1;
+            } else if (maxNum == arr[0]) {
+                if (minLen > arr[2] - arr[1] + 1) {
+                    minLen = arr[2] - arr[1] + 1;
+                }
+            }
+        }
+        return minLen;
     }
 }
