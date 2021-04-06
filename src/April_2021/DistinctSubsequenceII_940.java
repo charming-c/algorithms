1fa6a329160103540dba9628b5b86e1f41ef81fa
charming-c <822170415@qq.com>
2021-04-06 18:46:38 +0800

    4.06

--- src/April_2021/DistinctSubsequenceII_940.java
+++ src/April_2021/DistinctSubsequenceII_940.java
@@ -45,7 +45,7 @@ public class DistinctSubsequenceII_940 {
         public int distinctSubseqII(String S) {
             int MOD = 1_000_000_007;
             int N = S.length();
-            int[] dp = new int[N+1];
+            int[] dp = new int[N + 1];
             dp[0] = 1;
 
             int[] last = new int[26];
@@ -53,10 +53,10 @@ public class DistinctSubsequenceII_940 {
 
             for (int i = 0; i < N; ++i) {
                 int x = S.charAt(i) - 'a';
-                dp[i+1] = dp[i] * 2 % MOD;
+                dp[i + 1] = dp[i] * 2 % MOD;
                 if (last[x] >= 0)
-                    dp[i+1] -= dp[last[x]];
-                dp[i+1] %= MOD;
+                    dp[i + 1] -= dp[last[x]];
+                dp[i + 1] %= MOD;
                 last[x] = i;
             }
 
